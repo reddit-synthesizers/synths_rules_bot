0441aeb3a7ef4d147507cbcaa469039963ad2daa
SynthBot <synthbot@reddit.com>
2022-03-12 12:26:37 -0800

    Formatting. Minor cleanup.

--- synths_rules_bot.py
+++ synths_rules_bot.py
@@ -6,15 +6,21 @@ import threading
 from string import Template
 
 DEFAULT_SUBREDDIT_NAME = 'synthesizers'
-    
-MINUTES_TO_WARN = 5 # number of minutes before warning the user
-MINUTES_TO_REMOVE = 60 # number of minutes before removing the post if the user has not commented
-MIN_COMMENTERS_TO_KEEP = 5 # number of unique commenters to keep the post if the user has not commented
+
+# number of minutes before warning the user
+MINUTES_TO_WARN = 5
+# number of minutes before removing the post if the user has not commented
+MINUTES_TO_REMOVE = 60
+# number of unique commenters to keep the post if the user has not commented
+MIN_COMMENTERS_TO_KEEP = 5
+
 
 class SynthsRulesBot:
     def __init__(self, subreddit_name=DEFAULT_SUBREDDIT_NAME):
-        self.warning_template = Template(self.read_text_file('rule5-warning.txt'))
-        self.removal_template = Template(self.read_text_file('rule5-removal.txt'))
+        self.warning_template = Template(
+            self.read_text_file('rule5-warning.txt'))
+        self.removal_template = Template(
+            self.read_text_file('rule5-removal.txt'))
 
         self.reddit = praw.Reddit('SynthRulesBot')
         subreddit = self.reddit.subreddit(subreddit_name)
@@ -35,7 +41,7 @@ class SynthsRulesBot:
             elif age >= MINUTES_TO_WARN and not author_commented:
                 target = self.warning_worker
 
-            if not target == None:
+            if target != None:
                 thread = threading.Thread(target=target, args=(submission,))
                 thread.start()
 
@@ -43,45 +49,48 @@ class SynthsRulesBot:
         if not self.has_bot_comment(submission):
             messaage = self.warning_template.substitute(
                 author=submission.author.name, minutes=MINUTES_TO_REMOVE)
-            
+
             bot_comment = submission.reply(messaage)
             bot_comment.mod.distinguish(sticky=True)
             bot_comment.mod.ignore_reports()
-            
+
             self.log('Warned', submission)
 
     def remove_worker(self, submission):
         if self.get_unique_commenters_len(submission) >= MIN_COMMENTERS_TO_KEEP:
             self.log('Submission appears engaging, will not remove', submission)
         else:
-            submission.mod.remove(mod_note='Rule 5: OP did not comment, removed submission')
-            
+            submission.mod.remove(
+                mod_note='Rule 5: OP did not comment, removed submission')
+
             message = self.removal_template.substitute(
                 author=submission.author.name, minutes=MINUTES_TO_REMOVE)
             submission.mod.send_removal_message(message)
-            
-            self.log('Removed', submission);
+
+            self.log('Removed', submission)
 
     def cleanup_worker(self, submission):
         bot_comments = self.find_bot_comments(submission)
 
         for comment in bot_comments:
             if not comment.removed:
-                self.log('No longer actionable. Cleaning up bot comments', submission)
-                comment.mod.remove(mod_note='Rule 5: OP commented, removed warning')
+                self.log(
+                    'No longer actionable. Cleaning up bot comments', submission)
+                comment.mod.remove(
+                    mod_note='Rule 5: OP commented, removed warning')
 
     # 1. Not a self post
     # 2. Not locked
-    # 3. Not distingushed 
+    # 3. Not distingushed
     # 4. Not created by AutoModerator
     def is_submission_actionable(self, submission):
         return (not submission.is_self
-            and not submission.approved
-            and not submission.locked
-            and not submission.distinguished
-            and not submission.author.name == 'AutoModerator')
+                and not submission.approved
+                and not submission.locked
+                and not submission.distinguished
+                and not submission.author.name == 'AutoModerator')
 
-    # returns submission age in minutes    
+    # returns submission age in minutes
     # why does PRAW use local time for UTC?
     def get_submission_age(self, submission):
         now = datetime.datetime.now()
@@ -99,7 +108,7 @@ class SynthsRulesBot:
         for comment in flattened_comments:
             if comment.is_submitter:
                 author_commented = True
-                break 
+                break
 
         return author_commented
 
@@ -143,6 +152,7 @@ class SynthsRulesBot:
 
 if __name__ == '__main__':
     SynthsRulesBot()
+
 
 def lambda_handler(event, context):
     subreddit_name = os.environ['subreddit_name'] if 'subreddit_name' in os.environ else DEFAULT_SUBREDDIT_NAME
